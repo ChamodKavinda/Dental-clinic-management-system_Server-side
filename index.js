b101d7dd205adac2edc7efcd67df1fb88a4de08b
ChamodKavinda <kavindachamod100@gmail.com>
2024-05-29 22:01:39 +0530

    use cors to fixing Cross-Origin Resource Sharing policy restrictions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser')
 require('dotenv').config();
 
@@ -7,8 +8,11 @@ const port = process.env.PORT;
 
 const AppointmentRoute=require('./route/AppointmentRoute');
 
+
+
 const app = express();
 
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
