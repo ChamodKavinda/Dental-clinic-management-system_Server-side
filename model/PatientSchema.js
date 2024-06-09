2a7fa23ee36373281893e610855ca89ab1b66dd5
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-09 22:18:51 +0530

    Create patient Schema

---
+++ model/PatientSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const PatientSchema = new mongoose.Schema({
+    id:{type:String,require:true},
+    name:{type:String,require:true},
+    age:{type:Number,require:true},
+    number:{type:Number,require:true},
+    sex:{type:String,require:true},
+    address:{type:String,require:true},
+    description:{type:String,require:true},
+})
+
+module.exports=mongoose.model('patient',PatientSchema)
