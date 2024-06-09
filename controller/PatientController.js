941a8fc8435471c543fa47c985de123e02ed0386
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-09 22:41:37 +0530

    Implement Patient update controller

--- controller/PatientController.js
+++ controller/PatientController.js
@@ -17,3 +17,19 @@ const savePatient=(req,resp)=>{
         resp.status(500).json(error);
     })
 }
+
+const updatePatient=(req,resp)=>{
+    Patient.updateOne({id:req.body.id},{
+        id:req.body.id,
+        name:req.body.name,
+        age:req.body.age,
+        number:req.body.number,
+        sex:req.body.sex,
+        address:req.body.address,
+        description:req.body.description,
+    }).then(result=>{
+        resp.status(201).json(result);
+    }).catch(error=>{
+        resp.status(500).json(error);
+    })
+}
