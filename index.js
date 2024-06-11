084ebccae41ce67f2beb6f51aef35694477b9271
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-11 18:06:21 +0530

    use routes

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const authRoute = require("./auth/Routes/AuthRoute");
 const bodyParser = require('body-parser')
 require('dotenv').config();
+const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT;
 
 const AppointmentRoute=require('./route/AppointmentRoute');
-
-
+const PatientRoute=require('./route/PatientRoutes');
+const DentistRoute=require('./route/DentistRoute')
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173", // Allow requests from the frontend
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+}));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-
-app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/DCMS').then(()=>{
     app.listen(port, ()=>{
@@ -27,4 +32,7 @@ mongoose.connect('mongodb://localhost:27017/DCMS').then(()=>{
     console.log('Mongodb Error',error);
 })
 
-app.use('/api',AppointmentRoute);
+app.use('/appointment',AppointmentRoute);
+app.use('/patient',PatientRoute);
+app.use('/dentist',DentistRoute)
+app.use("/", authRoute);
