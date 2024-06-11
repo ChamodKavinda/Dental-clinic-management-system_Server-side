ca5499b4cb50527360b14fe3bc121cd4d7b187c7
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-11 11:15:03 +0530

    set routes for dentist

---
+++ route/DentistRoute.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const dentistController = require('../controller/DentistController');
+
+const router = express.Router();
+
+router.post('/save',dentistController.saveDentist);
+router.put('/update',dentistController.updateDentist);
+router.get('/get',dentistController.getAllDentist);
+router.delete('/delete',dentistController.deleteDentist);
+
+module.exports=router;
