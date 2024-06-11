9540dc9ebe7881aaedd84f9c7330b91ba18a8236
ChamodKavinda <kavindachamod100@gmail.com>
2024-06-11 10:48:53 +0530

    create save dentist controller

---
+++ controller/DentistController.js
@@ -0,0 +1,21 @@
+const Dentist = require('../model/DentistSchema');
+
+const saveDentist=(req,resp)=>{
+    const dentistDTO= new Dentist({
+        id:req.body.id,
+        name:req.body.name,
+        age:req.body.age,
+        number:req.body.number,
+        sex:req.body.sex,
+        address:req.body.address,
+        description:req.body.description
+    })
+
+    dentistDTO.save()
+        .then(result=>{
+            console.log(dentistDTO);
+            resp.status(201).json(result);
+        }).catch(error=>{
+            resp.status(500).json(error);
+    });
+}
