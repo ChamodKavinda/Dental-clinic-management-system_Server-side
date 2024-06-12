3e2ba55a4a65ed1d9bf2fa04a3914521b254aab1
chamod kavinda <kavindachamod100@gmail.com>
2024-06-13 00:57:08 +0530

    implement API for email deliver

---
+++ mailController.js
@@ -0,0 +1,29 @@
+const nodemailer = require('nodemailer');
+
+const emailSender = (req,resp)=> {
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'kaduwalihini@gmail.com',
+            pass: 'rpxt dvhk xqxx gffo'
+        }
+    });
+
+    var mailOptions = {
+        from: 'kaduwalihini@gmail.com',
+        to: req.body.email,
+        subject: req.body.subject,
+        text: req.body.message
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+            resp.status(200).send('Email sent');
+        }
+    });
+}
+
+module.exports={emailSender};
