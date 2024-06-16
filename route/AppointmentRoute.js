e988f72d1f89d99efdfbeff95ee6c5203bdb3529
chamod kavinda <kavindachamod100@gmail.com>
2024-06-17 01:20:52 +0530

    set route for mailController

--- route/AppointmentRoute.js
+++ route/AppointmentRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const appointmentController=require('../controller/AppointmentController');
-
 const router = express.Router();
+const mailController = require('../mailController');
 
 router.post('/save', appointmentController.saveAppointment);
 router.get('/get', appointmentController.getAppointment);
 router.put('/update', appointmentController.updateAppointment);
 router.delete('/delete', appointmentController.deleteAppointment);
+
+router.post("/sendEmail",mailController.emailSender);
+
 
 module.exports=router;
