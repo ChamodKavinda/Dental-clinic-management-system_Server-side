238ce3dd5a1d753883864beb3d739462d364601b
chamod kavinda <kavindachamod100@gmail.com>
2024-09-20 12:49:43 +0530

    change variable names

--- model/AppointmentSchema.js
+++ model/AppointmentSchema.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const AppointmentSchema = new mongoose.Schema({
    id:{type:String, require:true},
-   patient:{type:String, require:true},
-   dentist:{type:String, require:true},
+   patientId:{type:String, require:true},
+   dentistId:{type:String, require:true},
    date:{type:String, require:true},
    time:{type:String, require:true},
+   email:{type:String, require:true},
+   description:{type:String, require:true},
 });
 
 module.exports=mongoose.model('appointment',AppointmentSchema)
